0b707fee5bdd346aeacc033720e56263c3bbbe23
Your Name <nyiminhtet@Nyis-MacBook-Pro.local>
2025-12-19 14:29:20 +0700

    react-component assignment

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Greetings from './Greetings.jsx'
+import User from './User.jsx'
+
+function App() {
+  return (
+    <div className="container">
+      <Greetings />
+      <User name="Nyi Min Htet" />
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/Greetings.jsx
@@ -0,0 +1,5 @@
+function Greetings() {
+  return <h1>Hello, welcome to React!</h1>
+}
+
+export default Greetings
